b0b7cb960c3b9099b792d24791e352b865084813
Shen <shenj7@rose-hulman.edu>
2021-11-30 09:10:07 -0500

    inventory main class

--- GuitarStore/src/Inventory.java
+++ GuitarStore/src/Inventory.java
@@ -2,6 +2,10 @@
 public class Inventory {
 	Guitar[] guitarList;
 	boolean isEmployee;
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Rick's Guitar Shop!");
+	}
 
 	public void addGuitar(String serialNum, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
 
