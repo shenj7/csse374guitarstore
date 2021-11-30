e58edb3c64f29dcd18aaf4646382210fb77fa0f2
Shen <shenj7@rose-hulman.edu>
2021-11-30 09:01:13 -0500

    guitar and inventory

--- GuitarStore/src/Guitar.java
+++ GuitarStore/src/Guitar.java
@@ -1,4 +1,22 @@
 
 public class Guitar {
+	String serialNumber;
+	double price;
+	GuitarSpec spec;
 
+	public String getSerialNumber() {
+		return this.serialNumber;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public GuitarSpec getSpec() {
+		return this.spec;
+	}
 }
--- GuitarStore/src/Inventory.java
+++ GuitarStore/src/Inventory.java
@@ -1,4 +1,16 @@
 
 public class Inventory {
+	Guitar[] guitarList;
 
+	public void addGuitar(String serialNum, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+
+	}
+
+	public Guitar getGuitar(String specs) {
+		return new Guitar();
+	}
+
+	public Guitar[] search(GuitarSpec spec) {
+		return null;
+	}
 }
