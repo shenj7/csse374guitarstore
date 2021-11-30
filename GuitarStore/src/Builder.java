e9a70e604139fb0ffbab2b8e8f4fbd9ed2867a7d
Glacie0000 <35363328+Glacie0000@users.noreply.github.com>
2021-11-30 10:45:34 -0500

    Update Builder.java

--- GuitarStore/src/Builder.java
+++ GuitarStore/src/Builder.java
@@ -1,4 +1,16 @@
 
 public enum Builder {
-
+	GIBSON, HARMAN, SHURE, YAMAHA;
+	
+	@Override
+	public String toString() {
+		switch(this) {
+			case GIBSON: return "Gibson";
+			case HARMAN: return "Harman";
+			case SHURE: return "Shure";
+			case YAMAHA: return "Yamaha";
+			default: return "unspecified";
+		}
+	}
+	
 }
