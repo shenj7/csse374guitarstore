b37038258f1a72249c9f210f8e5b816a3d8b7905
Glacie0000 <35363328+Glacie0000@users.noreply.github.com>
2021-11-30 08:57:14 -0500

    created enums

--- GuitarStore/src/GuitarSpec.java
+++ GuitarStore/src/GuitarSpec.java
@@ -1,4 +1,24 @@
 
 public class GuitarSpec {
 
+	public enum Builder {
+		
+		
+//		@Override
+//		public String toString(){
+//			switch(this) {
+//				return "";
+//			}
+//		}
+		
+	}
+	
+	public enum Type {
+			
+	}
+	
+	public enum Wood {
+		
+	}
+	
 }
