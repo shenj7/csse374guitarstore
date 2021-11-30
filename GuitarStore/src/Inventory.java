87e4ab663e3babd5ffd2961b1aa01cc44eef6a72
Glacie0000 <35363328+Glacie0000@users.noreply.github.com>
2021-11-30 15:27:59 -0500

    add piano works

--- GuitarStore/src/Inventory.java
+++ GuitarStore/src/Inventory.java
@@ -1,8 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Inventory {
 	ArrayList<Guitar> guitarList;
-	ArrayList<Piano> pianoList;
+	static ArrayList<Piano> pianoList = new ArrayList<Piano>();
 	ArrayList<Drum> drumList;
 	boolean isEmployee;
 	String guitarFile;
@@ -11,6 +13,8 @@ public class Inventory {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Rick's Guitar Shop!");
+		//Just for test
+		addPiano("12345", 12.5, Builder.GIBSON, "some model", PianoType.CONSOLE, Wood.ALDER);
 	}
 
 	public void loadGuitars() {
@@ -27,10 +31,20 @@ public class Inventory {
 	
 	public void addGuitar(String serialNum, double price, Builder builder, String model, GuitarType type, Wood backWood, Wood topWood) {
 		guitarList.add(new Guitar(serialNum, price, builder, model, type, backWood, topWood));
+		
 	}
 	
-	public void addPiano(String serialNum, double price, Builder builder, String model, PianoType type, Wood wood) {
+	public static void addPiano(String serialNum, double price, Builder builder, String model, PianoType type, Wood wood) {
 		pianoList.add(new Piano(serialNum, price, builder, model, type, wood));
+		try {
+			FileWriter writer = new FileWriter("piano.txt");
+			writer.write(serialNum + " " + price + " " + builder.toString() + " " + model + " " + type.toString() + " " + wood.toString());
+			System.out.println(writer.toString());
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("file not exist");
+			e.printStackTrace();
+		}
 	}
 	
 	public void addDrum(String serialNum, double price, Builder builder, String model, DrumType type, Wood wood, DrumHead drumHead) {
